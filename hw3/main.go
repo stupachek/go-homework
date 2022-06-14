e1818b996f6e0ee01a1e847cc1020ab5344eeab8
stupachek <alona.stupak@gmail.com>
2022-06-14 16:27:10 +0300

    I think, Andrii hates me.

--- hw3/main.go
+++ hw3/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 const (
-	priceApple = 5.99
-	pricePear  = 7.
+	applePrice = 5.99
+	pearPrice  = 7.
 	yourMoney  = 23.
 )
 
@@ -15,23 +15,23 @@ func main() {
 	appleCount := 9
 	pearCount := 8
 	fmt.Printf("1. Скільки грошей треба витратити, щоб купити %d яблук та %d груш?\n", appleCount, pearCount)
-	costApples := float64(appleCount) * priceApple
-	costPears := float64(pearCount * pricePear)
-	fmt.Printf("\t- Вони коштують %vгрн.\n", costApples+costPears)
+	applesCost := float64(appleCount) * applePrice
+	pearCost := float64(pearCount * pearPrice)
+	fmt.Printf("\t- Вони коштують %vгрн.\n", applesCost+pearCost)
 
 	fmt.Println("2. Скільки груш ми можемо купити?")
-	pearsCanBuy := math.Floor(yourMoney / pricePear)
+	pearsCanBuy := math.Floor(yourMoney / pearPrice)
 	fmt.Printf("\t- Ми можемо купити %v.\n", pearsCanBuy)
 
-	applesCanBuy := math.Floor(yourMoney / priceApple)
+	applesCanBuy := math.Floor(yourMoney / applePrice)
 	fmt.Println("3. Скільки яблук ми можемо купити?")
 	fmt.Printf("\t- Ми можемо купити %v.\n", applesCanBuy)
 
-	nApples := 2
-	nPears := 2
-	fmt.Printf("4. Чи ми можемо купити %d груші та %d яблука?\n", nApples, nPears)
-	costOfnApples := float64(nApples) * priceApple
-	costOfnPears := float64(nPears) * priceApple
-	fmt.Printf("\t- %t\n", costOfnApples+costOfnPears <= yourMoney)
+	appleCount = 2
+	pearCount = 2
+	fmt.Printf("4. Чи ми можемо купити %d груші та %d яблука?\n", appleCount, pearCount)
+	nApplesCost := float64(appleCount) * applePrice
+	nPearsCost := float64(pearCount) * pearPrice
+	fmt.Printf("\t- %t\n", nApplesCost+nPearsCost <= yourMoney)
 
 }
